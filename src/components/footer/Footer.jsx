3f297666f094bbc2945aeee7759b5f9648389596
LERO <turgunboyevsamandar4@gmail.com>
2023-06-05 16:07:15 +0500

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,30 +1,53 @@
 import React from "react";
 import "./footer.scss";
 import logo from "../../assets/icons/icon-72x72.png";
-import { Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import { H1, H3, Paragraph } from "../../ui/typography";
 
 const Footer = () => {
   return (
-    <footer>
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <ul>
-          <li>
-            <img src={logo} />
-          </li>
-        </ul>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </Container>
-    </footer>
+    <>
+      <footer>
+        <Container
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            gap: "20px",
+            paddingBottom: '20px'
+          }}
+        >
+          <H1>
+            Focus on profit of your business with Brizo
+          </H1>
+          <H3>
+            Give your sales and performance a boost right now
+          </H3>
+          <Button
+            variant="contained"
+            color="white"
+            sx={{
+              color: "#fff",
+              boxShadow: "0px 15px 50px 0px rgba(0,12,33,0.2)",
+              marginTop: "40px",
+            }}
+          >
+            Bepul sinab ko'rish
+          </Button>
+          <Paragraph>30 kun davomida bepul</Paragraph>
+        </Container>
+      </footer>
+      <footer className="footer2">
+        <Container>
+            <ul>
+              <H3>Lorem, ipsum.</H3>
+              <H3>Lorem, ipsum.</H3>
+              <H3>Lorem, ipsum.</H3>
+            </ul>
+        </Container>
+      </footer>
+    </>
   );
 };
 
