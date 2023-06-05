2b09543df7d2d908c171ea09178691972c6f751f
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-06-05 10:38:10 +0500

    Files added

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At cupiditate autem omnis veritatis nesciunt dolores nam est, minima voluptatem repellat aut ea veniam atque! Facilis blanditiis nostrum eum esse possimus!
+    </div>
+  );
+}
+
+export default App;
