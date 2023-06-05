2d232914162a24fddbc3c0fa72384e899cec307b
Abror <135578437+eagleabror@users.noreply.github.com>
2023-06-05 11:46:17 +0500

    test

---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <ul>
+            <li>salom</li>
+            <li>salom</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
