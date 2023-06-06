af63b5b33ccbb1fb0ec1c2fbb89923e7ffca0111
LERO <turgunboyevsamandar4@gmail.com>
2023-06-06 17:02:21 +0500

    new footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,60 +1,63 @@
 import React from "react";
-import { Button, Container } from "@mui/material";
-import { H1, H3, Paragraph } from "../../ui/typography";
+import { Box, Button, Container } from "@mui/material";
+import { H1, H2, H3, Paragraph } from "../../ui/typography";
 import { motion } from "framer-motion";
+import telegramicon from "../../assets/icons/telegramicon.png";
+import instagramicon from "../../assets/icons/instagramicon.png";
 import "./footer.scss";
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   return (
     <>
       <footer className="footer1">
-        <Container className="Container"
+        <Container
+          className="Container"
           sx={{
-            padding: "10px 0"
+            padding: "10px 0",
           }}
         >
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.5, type: "spring" }}
-          >
-            <H3>{t("footer-title")}</H3>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1.5, y: 0 }}
-            transition={{ duration: 1, type: "spring", delay: 0.2 }}
-          >
-            <H3>{t("footer-paragraph")}</H3>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, scale: 0.7 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 2, type: "spring", delay: 0.4 }}
-          >
-            <Button
-              variant="contained"
-              color="white"
-              sx={{
-                color: "#fff",
-                boxShadow: "0px 15px 50px 0px rgba(0,12,33,0.2)",
-                marginTop: "30px",
-              }}
+          <div className="info">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.5, type: "spring" }}
             >
-              {t("button")}
-            </Button>
-          </motion.div>
-          <Paragraph>{t("button-title")}</Paragraph>
+              <H2>{t("footer-title")}</H2>
+              <H3>+998 78 777 11 00</H3>
+              <H3>{t("footer-title-city")}</H3>
+            </motion.div>
+          </div>
+          <div className="footer-links">
+            <H2>{t("footer-generelt")}</H2>
+            <H3>{t("footer-generelt-tab1")}</H3>
+            <H3>{t("footer-generelt-tab2")}</H3>
+            <H3>{t("footer-generelt-tab3")}</H3>
+          </div>
+          <div className="footer-support">
+            <H2>{t("footer-support")}</H2>
+            <H3>{t("footer-conditions")}</H3>
+            <H3>{t("footer-confidentiality")}</H3>
+            <H3>{t("footer-map")}</H3>
+          </div>
+          <div className="support-social">
+            <H2>{t("footer-social")}</H2>
+            <Paragraph
+              style={{ display: "flex", alignItems: "center", gap: "5px" }}
+            >
+              <img src={telegramicon} /> Telegram
+            </Paragraph>
+            <Paragraph
+              style={{ display: "flex", alignItems: "center", gap: "10px" }}
+            >
+              <img src={instagramicon} /> instagram
+            </Paragraph>
+          </div>
         </Container>
       </footer>
-      <footer className="footer2">
-        <Container className="footercontainer">
-          <Paragraph>{t("footer-conditions")}</Paragraph>
-          <Paragraph>{t("footer-confidentiality")}</Paragraph>
-          <Paragraph>{t("footer-map")}</Paragraph>
-        </Container>
+      <footer className="footer-container">
+          <H3>@ 2023 {t("footer-title")}</H3>
       </footer>
     </>
   );
