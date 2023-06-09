8e728d9fbf775a374fa174885439eb361373aadb
Abror <abrorkhandeveloper@gmail.com>
2023-06-09 19:33:52 +0500

    login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from "framer-motion";
+import { Box, Button } from '@mui/material';
+import { H1 } from '../../ui/typography';
+import { t } from 'i18next';
+import InputComp from '../../ui/InputComp';
+import { useTranslation } from 'react-i18next';
+import { RegisterBox } from '../register/Registerstyled';
+import { LoginBox } from './Loginstyled';
 
 const Login = () => {
+  const { t } = useTranslation();
+  const [phone, setPhone] = useState("");
+  const [paswword, setPaswword] = useState("");
   return (
-    <div>Login Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae culpa nobis nemo officiis! A, tempora officia. Maxime labore nam adipisci sint, quia voluptas veniam magni similique! Ab asperiores quis non doloribus recusandae dicta, exercitationem numquam, doloremque est a earum cum odit nisi porro dolorem, consequuntur veniam repudiandae incidunt fugiat tempore? In, quam, officia sed, illo sapiente debitis cumque eos ut iste quia veniam magni magnam itaque beatae reprehenderit distinctio excepturi eaque corporis voluptatem aperiam cum rerum maxime reiciendis. Voluptatibus sequi nemo doloremque quo molestiae est vero. Ab sit quisquam culpa repudiandae neque quos corporis, animi provident aliquam repellendus blanditiis error.</div>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.7 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.6, type: "spring", delay: 0.4 }}
+    >
+
+      <LoginBox>
+        <H1 className="register-heading" style={{ padding: "1rem" }}>{t("login")}</H1>
+        <Box
+          sx={{
+            width: { xs: "100%", md: "60%" },
+            margin: "0 auto",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+        >
+          <InputComp
+            placeholder="+998 78 777 11 00"
+            value={phone}
+            setValue={setPhone}
+            label={t("register-phone")}
+            required={true}
+          />
+          <InputComp
+            placeholder="12345678"
+            value={paswword}
+            setValue={setPaswword}
+            label={t("register-paswword")}
+            required={true}
+          />
+
+          <Button
+            variant="contained"
+            color="blue"
+            sx={{
+              color: "#fff",
+              width: "100%",
+              boxShadow: "0px 15px 50px 0px rgba(0,12,33,0.2)",
+            }}
+          >
+            {t("login")}
+          </Button>
+
+        </Box>
+      </LoginBox>
+    </motion.div>
   )
 }
 
---
+++ src/components/login/Loginstyled.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const LoginBox = styled('div')(({ theme }) => ({
+    width: {xs: "800px"},
+    margin: "0 auto",
+    height: "100vh",
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+}))
