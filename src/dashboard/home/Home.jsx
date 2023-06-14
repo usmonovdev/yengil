15aa99f0e79dd982574827060916749a9c30c9f1
Abror <abrorkhandeveloper@gmail.com>
2023-06-14 20:15:55 +0500

    animation

--- src/dashboard/home/Home.jsx
+++ src/dashboard/home/Home.jsx
@@ -49,8 +49,8 @@ const Home = () => {
       >
         <StatBox>
           <motion.div
-            initial={{ opacity: 0, scale: 0.7 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, translateY: 10 }}
+            animate={{ opacity: 1, translateY: 0,  }}
             transition={{ duration: 1, type: "spring", delay: 0.4 }}
           >
             <Divs>
@@ -127,8 +127,8 @@ const Home = () => {
             </Divs>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, scale: 0.7 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, translateY: 10 }}
+            animate={{ opacity: 1, translateY: 0 }}
             transition={{ duration: 1.5, type: "spring", delay: 0.4 }}
           >
 
@@ -249,8 +249,8 @@ const Home = () => {
             </Divs>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, scale: 0.7 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, translateY: 10}}
+            animate={{ opacity: 1, translateY: 0 }}
             transition={{ duration: 2, type: "spring", delay: 0.4 }}
           >
 
