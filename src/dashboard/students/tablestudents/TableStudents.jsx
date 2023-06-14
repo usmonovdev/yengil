6e7919a6373eb9cfc83183189c5b7cd3e219ca80
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-06-14 23:57:40 +0500

    Students test

--- src/dashboard/students/tablestudents/TableStudents.jsx
+++ src/dashboard/students/tablestudents/TableStudents.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import { H2, Paragraph } from "../../../ui/typography";
-import { Box, Button, InputAdornment, Select, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  Paper,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TextField,
+  styled,
+  tableCellClasses,
+} from "@mui/material";
 import filters from "../../../assets/icons/filter.png";
 import filtersDark from "../../../assets/dark/fi-rr-filter.png";
 import { Img, TD } from "./TableStyled";
@@ -11,99 +27,150 @@ import { students } from "../../../localData/studentData";
 import NewStudents from "../new/NewStudents";
 import { useTheme } from "@emotion/react";
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.blue.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
 const TableStudents = () => {
   const [search, setSearch] = useState();
   const [filter, setFilter] = useState();
   const [sorting, setSorting] = React.useState("");
-  const theme = useTheme()
-  console.log(theme.palette.custom.whiteSmoke);
+  const theme = useTheme();
 
   const handleChange = (event) => {
     setSorting(event.target.value);
   };
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   return (
-    <>
-    <NewStudents/>
-      <div
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+      }}
+    >
+      <H2>O’quvchilar</H2>
+      <Paragraph>Jami - 1000</Paragraph>
+      <form
         style={{
-          width: "100%",
           display: "flex",
-          flexDirection: "column",
-          gap: "10px",
+          gap: "20px",
+          overflow: "scroll",
+          justifyContent: "space-between",
+          width: "1400px",
+        }}
+      >
+        <Box sx={{ display: "flex", gap: "20px" }}>
+          <TextField
+            label="Qidirish"
+            id="filled-start-adornment"
+            sx={{ width: "20ch", background: theme.palette.custom.whiteSmoke }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Img
+                    src={theme.palette.mode == "light" ? filters : filtersDark}
+                  />
+                </InputAdornment>
+              ),
+            }}
+            variant="filled"
+          />
+          <Box sx={{ width: "203px", height: "20x", display: "flex" }}>
+            <FormControl fullWidth sx={{ width: { xs: "100%" } }}>
+              <InputLabel id="demo-simple-select-label">Saralash</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Saralash"
+                value={sorting}
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Matem</MenuItem>
+                <MenuItem value={20}>Ona tili</MenuItem>
+                <MenuItem value={30}>Kimyo</MenuItem>
+                <MenuItem value={40}>Fizika</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Box>
+      </form>
+      <Button variant="contained" color="blue">
+        Qo'shish
+      </Button>
+      <Box
+        sx={{
+          width: "100%",
           overflow: "scroll",
         }}
       >
-        <H2>O’quvchilar</H2>
-        <Paragraph>Jami - 1000</Paragraph>
-        <form
-          style={{
-            display: "flex",
-            gap: "20px",
-            overflow: "scroll",
-            justifyContent: "space-between",
-            width: "1400px",
-          }}
-        >
-          <Box sx={{ display: "flex", gap: "20px" }}>
-            <TextField
-              label="Qidirish"
-              id="filled-start-adornment"
-              sx={{ width: "20ch", background: theme.palette.custom.whiteSmoke }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Img src={theme.palette.mode == "light" ? filters : filtersDark} />
-                  </InputAdornment>
-                ),
-              }}
-              variant="filled"
-            />
-            <Box sx={{ width: "203px", height: "20x", display: "flex" }}>
-              <FormControl fullWidth sx={{ width: { xs: "100%" } }}>
-                <InputLabel id="demo-simple-select-label">Saralash</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Saralash"
-                  value={sorting}
-                  onChange={handleChange}
-                >
-                  <MenuItem value={10}>Matem</MenuItem>
-                  <MenuItem value={20}>Ona tili</MenuItem>
-                  <MenuItem value={30}>Kimyo</MenuItem>
-                  <MenuItem value={40}>Fizika</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-          </Box>
-          <Button variant="contained" sx={{background: theme.palette.custom.bunting}}>Qo'shish</Button>
-        </form>
-        <>
-          <table
-            style={{ width: "1400px", padding: "10px", overflow: "scroll" }}
-          >
-            <tr>
-              <th style={{ width: "5%", textAlign: "center" }}></th>
-              <th>Ism familiya</th>
-              <th>Guruh</th>
-              <th>Telefon</th>
-              <th>To’lov</th>
-            </tr>
-            {students.map((student) => {
-              return (
-                <tr>
-                  <TD style={{ textAlign: "center" }}>{student.id}</TD>
-                  <TD>{student.name}</TD>
-                  <TD>{student.group}</TD>
-                  <TD>{student.tel}</TD>
-                  <TD>{student.payment}</TD>
-                </tr>
-              );
-            })}
-          </table>
-        </>
-      </div>
-    </>
+        <TableContainer>
+          <Table sx={{ minWidth: 700 }}>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align="left">Ism Familiya</StyledTableCell>
+                <StyledTableCell align="left">Telefon</StyledTableCell>
+                <StyledTableCell align="left">Guruh</StyledTableCell>
+                <StyledTableCell align="left">To'lov</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {students.map((student) => {
+                return (
+                  <StyledTableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={student.id}
+                  >
+                    <TableCell>{student.name}</TableCell>
+                    <TableCell>{student.tel}</TableCell>
+                    <TableCell>{student.group}</TableCell>
+                    <TableCell>{student.payment}</TableCell>
+                  </StyledTableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={students.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Box>
+    </Box>
   );
 };
 
--- src/localData/studentData.js
+++ src/localData/studentData.js
@@ -1,23 +1,100 @@
 export const students = [
     {
         name: 'Usmonov Azizbek',
-        tel: "+998910197803",
+        tel: "+99891 019 78 03",
         group: "Dasturlash",
         payment: "320.000",
         id:"1"
     },
     {
         name: 'Usmonov Azizbek',
-        tel: "+998910197803",
+        tel: "+99891 019 78 03",
         group: "Dasturlash",
         payment: "320.000",
         id:"2"
     },
     {
         name: 'Usmonov Azizbek',
-        tel: "+998910197803",
+        tel: "+99891 019 78 03",
         group: "Dasturlash",
         payment: "320.000",
         id:"3"
-    }
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"4"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"5"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"6"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"7"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"8"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"9"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"10"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"11"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"12"
+    },
+    {
+        name: 'Usmonov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"13"
+    },
+    {
+        name: 'Usmoasdaasnov Azizbek',
+        tel: "+99891 019 78 03",
+        group: "Dasturlash",
+        payment: "320.000",
+        id:"14"
+    },
 ]
