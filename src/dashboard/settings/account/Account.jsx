868aeb7544bfdde0b98dcfcc9776f1a77e2dbb30
Abror <abrorkhandeveloper@gmail.com>
2023-06-27 10:55:11 +0500

    account new

--- src/dashboard/settings/account/Account.jsx
+++ src/dashboard/settings/account/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import {
   Box,
@@ -18,7 +18,7 @@ import InputComp from "../../../ui/InputComp";
 import { IMaskInput } from "react-imask";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useTheme } from "@emotion/react";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { useTranslation } from "react-i18next";
 
 const TextMaskCustom = React.forwardRef(function TextMaskCustom(props, ref) {
@@ -75,102 +75,110 @@ const Account = ({ value, index }) => {
   };
   const { t } = useTranslation();
 
+  const ref = useRef();
+  const isInView = useInView(ref, { once: true });
+
   return (
     <TabPanel value={value} index={index}>
-      <motion.div
-        initial={{x: "30%" }}
-        animate={{x:-10}}
-        transition={{ duration: 0.9, delay: 0.1 }}
+      <Box
+        sx={{
+          transform: isInView ? "none" : "translateX(200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1)",
+        }}
+        ref={ref}
       >
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          gap: "20px",
-          justifyContent: "flex-start",
-        }}
-        >
+
         <Box
           sx={{
-            border: "2px solid #64b5f6",
-            width: "100px",
-            height: "100px",
-            borderRadius: "200px",
             display: "flex",
+            flexDirection: "row",
             alignItems: "center",
-            justifyContent: "center",
+            gap: "10px",
+            justifyContent: "flex-start",
           }}
           >
-          <img src={icon} style={{ width: "60px", height: "60px" }} />
-        </Box>
-        <H2>{t("settingsAccountLogo")}</H2>
-        <label htmlFor="logo">
-          <EditIcon />
-        </label>
-        <TextField
-          type="file"
-          sx={{ display: "none" }}
-          inputProps={{ accept: "image/*" }}
-          id="logo"
-          />
-      </Box>
-      <Box
-        sx={{
-          width: "300px",
-          marginTop: "50px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-          alignItems: "flex-end",
-        }}
-        >
-        <InputComp
-          label={t("settingsAccountCenterName")}
-          placeholder={"Buxoro scholl"}
-          value={name}
-          setValue={setName}
-          />
-        <InputComp
-          label={t("settingsAccountPhone")}
-          placeholder={"+998-90-000-00-00"}
-          value={phone}
-          setValue={setPhone}
-          inputProps={TextMaskCustom}
-          />
-        <FormControl
-          variant="outlined"
-          sx={{ width: "100%" }}
-          color="blue"
-          onClick={(e) => setPassword(e.target.value)}
-          >
-          <InputLabel htmlFor="outlined-adornment-password">
-            {t("settingsAccountPassword")}
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                  >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label={t("settingsAccountPassword")}
+          <Box
+            sx={{
+              border: "2px solid",
+              borderColor: "blue.main",
+              width: "80px",
+              height: "80px",
+              borderRadius: "200px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            >
+            <img src={icon} style={{ width: "55px", height: "55px" }} />
+          </Box>
+          <H2 style={{padding: 0}}>{t("settingsAccountLogo")}</H2>
+          <label htmlFor="logo">
+            <EditIcon style={{cursor: "pointer"}} />
+          </label>
+          <TextField
+            type="file"
+            sx={{ display: "none" }}
+            inputProps={{ accept: "image/*" }}
+            id="logo"
             />
-        </FormControl>
-        <Button variant="contained" color="blue" sx={{ width: "50%" }}>
-          {t("settingsAccountButton")}
-        </Button>
+        </Box>
+        <Box
+          sx={{
+            width: "300px",
+            marginTop: "20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            alignItems: "flex-end",
+          }}
+          >
+          <InputComp
+            label={t("settingsAccountCenterName")}
+            placeholder={"Buxoro scholl"}
+            value={name}
+            setValue={setName}
+            />
+          <InputComp
+            label={t("settingsAccountPhone")}
+            placeholder={"+998-90-000-00-00"}
+            value={phone}
+            setValue={setPhone}
+            inputProps={TextMaskCustom}
+            />
+          <FormControl
+            variant="outlined"
+            sx={{ width: "100%" }}
+            color="blue"
+            onClick={(e) => setPassword(e.target.value)}
+            >
+            <InputLabel htmlFor="outlined-adornment-password">
+              {t("settingsAccountPassword")}
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label={t("settingsAccountPassword")}
+              />
+          </FormControl>
+          <Button variant="contained" color="blue" sx={{ width: "50%" }}>
+            {t("settingsAccountButton")}
+          </Button>
+        </Box>
       </Box>
-      </motion.div>
     </TabPanel>
   );
 };
