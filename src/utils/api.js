ac32df40d0992a3577459a992bbd0effc902d66e
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-08-29 23:57:35 +0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://nest-yengil-app.onrender.com/api"
+axios.defaults.baseURL = "https://yengil-app.onrender.com/api"
 
 // axios.interceptors.request.use(parametr => {
 //     const token = JSON.parse(localStorage.getItem("TOKEN"))
